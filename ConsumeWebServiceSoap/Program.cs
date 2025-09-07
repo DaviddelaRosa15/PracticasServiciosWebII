f4dcd189fba49a6da60b5aab952f4ac16515eaf7
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-09-07 14:39:59 -0400

    Add SOAP web service consumer for calculator API
    Initial implementation of a .NET console app that consumes a SOAP calculator web service. Includes service reference, project setup, and a program to read two numbers from the user and display their sum using the web service.

---
+++ ConsumeWebServiceSoap/Program.cs
@@ -0,0 +1,23 @@
+﻿using ServiceReference1;
+// See https://aka.ms/new-console-template for more information
+
+// Leer el primer numero
+Console.Write("Ingrese el 1er numero: ");
+int first = int.Parse(Console.ReadLine());
+
+// Leer el segundo numero
+Console.Write("Ingrese el 2do numero: ");
+int second = int.Parse(Console.ReadLine());
+
+// Imprimir una linea en blanco
+Console.WriteLine("");
+
+// Consumir el servicio web
+await ConsumeWebServiceSumAsync(first, second);
+
+static async Task ConsumeWebServiceSumAsync(int first, int second)
+{
+    var client = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+    int result = await client.AddAsync(first, second);
+    Console.WriteLine($"La suma es: {result}");
+}
